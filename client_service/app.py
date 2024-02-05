b88ade052f7f512a72abadbfaaa65f34fb3ffc7c
Brian Mak <bm323@imperial.ac.uk>
2024-02-05 16:31:23 +0000

    html

--- client_service/app.py
+++ client_service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import requests
 
 app = Flask(__name__)
@@ -10,6 +10,9 @@ def get_data_from_api(api_url, query_params=None):
 
         data = response.json()
 
+        return data
+
+        '''
         if query_params:
             filtered_data = []
             for key, value in query_params.items():
@@ -22,6 +25,8 @@ def get_data_from_api(api_url, query_params=None):
             return filtered_data
         else:
             return data
+        '''
+
     except requests.exceptions.RequestException as e:
         print(f"Error making API request: {e}")
         return None
@@ -42,3 +47,37 @@ def get_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/search_result", methods=["POST"])
+def search_result():
+
+    query_params = {key: value for key, value in {
+        "author": request.form.get("author"),
+        "genre": request.form.get("genre"),
+        "id": request.form.get("id"),
+        "publication_year": request.form.get("publication_year"),
+        "title": request.form.get("title")
+    }.items() if value is not ''}
+
+    print(query_params)
+    
+    try:
+        result = get_data_from_api('http://book-api-server-dns.cye8ahgvh7b6dkea.uksouth.azurecontainer.io:5000/books'
+                        , query_params=query_params)
+        
+        if len(result) > 1 or result == []: return "Invalid search input. Please try again." 
+
+        else: return render_template("search_result.html", author=result[0]["author"],
+                        genre=result[0]["genre"], id=result[0]["id"],
+                        publication_year=result[0]["publication_year"],
+                        title=result[0]["title"])
+    
+    except Exception as e:
+        return "Unknown Error. Please try again."
+      
+
+
